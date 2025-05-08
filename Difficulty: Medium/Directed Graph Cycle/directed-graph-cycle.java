83b2ba767f5f857473042a74b51b901055c13ede
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-08 09:17:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            int[][] edges = new int[E][2];
+            for (int i = 0; i < E; i++) {
+                edges[i][0] = sc.nextInt();
+                edges[i][1] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            boolean ans = obj.isCyclic(V, edges);
+            System.out.println(ans ? "true" : "false");
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    boolean[] visited;
+    boolean[] isRecurrsion;
+    public boolean isCyclic(int V, int[][] edges) {
+        // code here
+        Map<Integer,List<Integer>> map=new HashMap<>();
+        for(int[] edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            if(!map.containsKey(u)) map.put(u,new ArrayList<>());
+            map.get(u).add(v);
+        }
+        visited=new boolean[V];
+        isRecurrsion=new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!visited[i] && isCycle(map,i)) return true;
+        }
+        return false;
+    }
+    private boolean isCycle(Map<Integer,List<Integer>> map, int u){
+        visited[u]=true;
+        isRecurrsion[u]=true;
+        for(int v:map.getOrDefault(u,new ArrayList<>())){
+            if(visited[v]==false && isCycle(map,v)) return true;
+            else if(isRecurrsion[v])    return true;
+        }
+        isRecurrsion[u]=false;
+        return false;
+    }
+}
