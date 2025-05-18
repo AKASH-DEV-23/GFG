38e22ba4867cce14093cb71ffa3675bb140f8cd7
AKASH KUMAR <164494409+AKASH-DEV-23@users.noreply.github.com>
2025-05-18 15:10:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -24,33 +24,75 @@ class GFG {
 }
 // } Driver Code Ends
 
+        // USING DFS
+        
+// class Solution {
+//     boolean[] visited;
+//     boolean[] isRecurrsion;
+//     public boolean isCyclic(int V, int[][] edges) {
+//         // code here
+//         Map<Integer,List<Integer>> map=new HashMap<>();
+//         for(int[] edge:edges){
+//             int u=edge[0];
+//             int v=edge[1];
+//             if(!map.containsKey(u)) map.put(u,new ArrayList<>());
+//             map.get(u).add(v);
+//         }
+//         visited=new boolean[V];
+//         isRecurrsion=new boolean[V];
+//         for(int i=0;i<V;i++){
+//             if(!visited[i] && isCycle(map,i)) return true;
+//         }
+//         return false;
+//     }
+//     private boolean isCycle(Map<Integer,List<Integer>> map, int u){
+//         visited[u]=true;
+//         isRecurrsion[u]=true;
+//         for(int v:map.getOrDefault(u,new ArrayList<>())){
+//             if(visited[v]==false && isCycle(map,v)) return true;
+//             else if(isRecurrsion[v])    return true;
+//         }
+//         isRecurrsion[u]=false;
+//         return false;
+//     }
+// }
+
+
+      // USING BFS KAHN'S ALGORITHM
+      
 class Solution {
-    boolean[] visited;
-    boolean[] isRecurrsion;
     public boolean isCyclic(int V, int[][] edges) {
         // code here
-        Map<Integer,List<Integer>> map=new HashMap<>();
-        for(int[] edge:edges){
-            int u=edge[0];
-            int v=edge[1];
-            if(!map.containsKey(u)) map.put(u,new ArrayList<>());
-            map.get(u).add(v);
-        }
-        visited=new boolean[V];
-        isRecurrsion=new boolean[V];
-        for(int i=0;i<V;i++){
-            if(!visited[i] && isCycle(map,i)) return true;
-        }
-        return false;
-    }
-    private boolean isCycle(Map<Integer,List<Integer>> map, int u){
-        visited[u]=true;
-        isRecurrsion[u]=true;
-        for(int v:map.getOrDefault(u,new ArrayList<>())){
-            if(visited[v]==false && isCycle(map,v)) return true;
-            else if(isRecurrsion[v])    return true;
-        }
-        isRecurrsion[u]=false;
-        return false;
+       Map<Integer, List<Integer>> map=new HashMap<>();
+       for(int edge[]:edges){
+           int u=edge[0];
+           int v=edge[1];
+           if(!map.containsKey(u))  map.put(u,new ArrayList<>());
+           map.get(u).add(v);
+       }
+       int cnt=0;
+       int[] inDegree=new int[V];
+       for(int key:map.keySet()){
+           for(int val:map.get(key))
+                inDegree[val]++;
+       }
+       Queue<Integer> q=new LinkedList<>();
+       for(int i=0;i<V;i++){
+           if(inDegree[i]==0){
+                q.offer(i);
+                cnt++;
+           }
+       }
+       while(!q.isEmpty()){
+           int u=q.poll();
+           for(int v:map.getOrDefault(u,new ArrayList<>())){
+               inDegree[v]--;
+               if(inDegree[v]==0){
+                   q.offer(v);
+                   cnt++;
+               }
+           }
+       }
+       return cnt==V ? false : true;
     }
 }
